3e8d851e4ce9df6d44f146f55e50a25b1e330dac
Olimpia Spyra <olimpia.spyra@gmail.com>
2021-09-05 17:10:18 +0200

    Add app

---
+++ client/app.js
@@ -0,0 +1,51 @@
+const loginForm = document.getElementById('welcome-form');
+const messagesSection = document.getElementById('messages-section');
+const messagesList = document.getElementById('messages-list');
+const addMessageForm = document.getElementById('add-messages-form');
+const userNameInput = document.getElementById('username');
+const messageContentInput = document.getElementById('message-content');
+
+let userName = '';
+
+ const login = (event) => {
+     event.preventDefault();
+     if (userNameInput.value === '') {
+         alert ('Please enter your login');
+    }
+        else {
+            userName = userNameInput.value;
+            loginForm.classList.remove('show');
+            messagesSection.classList.add('show');
+        }
+};
+
+loginForm.addEventListener('submit', login);
+
+const addMessage = (author, content) => {
+  const message = document.createElement('li');
+  message.classList.add('message');
+  message.classList.add('message--received');
+
+  if (author === userName) {
+    message.classList.add('.message--self');
+    message.innerHTML = `
+    <h3 class="message__author">${userName === author ? 'You' : author }</h3>
+    <div class="message__content">
+      ${content}
+    </div>
+  `;
+  messagesList.appendChild(message);
+  }
+}
+
+const sendMessage = (event) => {
+  event.preventDefault();
+  if (addMessageForm === '') {
+    alert ('Please type your message') 
+  } else {
+    addMessage(userName, messageContentInput.value);
+    messageContentInput.value = '';
+  }
+}
+
+addMessageForm.addEventListener('submit', sendMessage);
